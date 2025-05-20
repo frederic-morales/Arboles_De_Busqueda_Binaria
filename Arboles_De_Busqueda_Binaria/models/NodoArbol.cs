e9ae40bddfcbfad4e52726ca6d6ae2e58d0dddc5
Frederic Morales <tocomorales30@gmail.com>
2025-05-19 21:42:32 -0600

    first commit

---
+++ Arboles_De_Busqueda_Binaria/models/NodoArbol.cs
@@ -0,0 +1,14 @@
+﻿namespace Arboles_De_Busqueda_Binaria.models
+{
+    public class NodoArbol
+    {
+        public int Valor { get; set; }
+        public NodoArbol Izquierda { get; set; }
+        public NodoArbol Derecha { get; set; }
+
+        public NodoArbol(int valor)
+        {
+            Valor = valor;
+        }
+    }
+}
